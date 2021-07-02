763f2f22a3d74c4483b7fbca4a1df43ac200ae09
TheCodeYoda <smurf61988@gmail.com>
2021-07-02 13:47:04 +0530

    added velocity fields and update fn to update positions as time passes

--- src/circle.rs
+++ src/circle.rs
@@ -2,11 +2,19 @@ pub struct Circle {
     x: f64,
     y: f64,
     r: f64,
+    vel_x: f64,
+    vel_y: f64,
 }
 
 impl Circle {
-    pub fn new(x: f64, y: f64, r: f64) -> Circle {
-        return Circle { x: x, y: y, r: r };
+    pub fn new(x: f64, y: f64, r: f64, vel_x: f64, vel_y: f64) -> Circle {
+        return Circle {
+            x: x,
+            y: y,
+            r: r,
+            vel_x: vel_x,
+            vel_y: vel_y,
+        };
     }
     pub fn x(&self) -> f64 {
         return self.x;
@@ -18,5 +26,17 @@ impl Circle {
 
     pub fn r(&self) -> f64 {
         return self.r;
+    }
+
+    pub fn vel_x(&self) -> f64 {
+        return self.vel_x;
+    }
+    pub fn vel_y(&self) -> f64 {
+        return self.vel_y;
+    }
+
+    pub fn update_pos(&mut self, dt: f64) {
+        self.x += self.vel_x * dt;
+        self.y += self.vel_y * dt;
     }
 }
