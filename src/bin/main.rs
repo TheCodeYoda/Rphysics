905397312dc3622ce578db578def2a8e6c2cc01a
TheCodeYoda <smurf61988@gmail.com>
2021-07-02 13:47:39 +0530

    added update event to update the simulation after unit time dt

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -30,28 +30,45 @@ use rphysics::circle::*;
 // }
 
 fn get_circles() -> Vec<Circle> {
-    let circ_1 = Circle::new(0.0, 400.0, 100.0);
-    let circ_2 = Circle::new(400.0, 400.0, 100.0);
+    let circ_1 = Circle::new(0.0, 400.0, 100.0, 20.0, 0.0);
+    let circ_2 = Circle::new(400.0, 400.0, 100.0, -20.0, 0.0);
     return vec![circ_1, circ_2];
+}
+
+fn update(circ_list: &mut Vec<Circle>, dt: f64) {
+    for circ in circ_list {
+        circ.update_pos(dt);
+    }
 }
 
 fn main() {
     let mut window: PistonWindow = WindowSettings::new("Hello World!", [512; 2])
         .build()
         .unwrap();
+
+    // list of circles to render
+    let mut circ_list = get_circles();
     // render loop
     while let Some(e) = window.next() {
-        window.draw_2d(&e, |c, g, _| {
-            clear([0.5, 0.5, 0.5, 1.0], g);
-            let circ_list = get_circles();
-            for circ in circ_list {
-                ellipse(
-                    [1.0, 0.0, 0.0, 1.0],                     // red color
-                    [circ.x(), circ.y(), circ.r(), circ.r()], // rectangle  [x,y,width,height]
-                    c.transform,
-                    g,
-                );
-            }
-        });
+        // this is for rendering
+        if let Some(_) = e.render_args() {
+            window.draw_2d(&e, |c, g, _| {
+                clear([0.5, 0.5, 0.5, 1.0], g);
+                for circ in &circ_list {
+                    ellipse(
+                        [1.0, 0.0, 0.0, 1.0],                     // red color
+                        [circ.x(), circ.y(), circ.r(), circ.r()], // rectangle  [x,y,width,height]
+                        c.transform,
+                        g,
+                    );
+                }
+            });
+        }
+        // this is for updation of movement of shapes
+        if let Some(u) = e.update_args() {
+            // update position according to speed after every unit of time in simulation
+            // u->update object ;;;; u.dt ----> time elapsed in simulation
+            update(&mut circ_list, u.dt);
+        }
     }
 }
