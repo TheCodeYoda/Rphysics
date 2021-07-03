c1e883c03db6b538483dbfd5ad2c673f374cb404
TheCodeYoda <smurf61988@gmail.com>
2021-07-03 19:23:14 +0530

    fixed circle creation, now given x,y,r creates circle with radius r centered at (x,y) had to tweak ellipse function

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,53 +1,34 @@
 use piston_window::*;
 use rphysics::circle::*;
-// use rand::*;
-
-// const HEIGHT: f64 = 720.0;
-// const WIDTH: f64 = 1280.0;
-
-// struct Bubble {
-//     speed: f64,
-//     x: f64,
-//     y: f64,
-//     r: f64,
-// }
-
-// impl Bubble {
-//     fn new(num: Option<f64>) -> Bubble {
-//         let r: f64 = (random::<f64>() * WIDTH / 8.0) + 5.0;
-//         let mut b: Bubble = Bubble {
-//             speed: (random::<f64>() * (WIDTH / 8.0)) + 10.0,
-//             y: random::<f64>() * (HEIGHT + r),
-//             x: random::<f64>() * WIDTH,
-//             r: r,
-//         };
-//         if let Some(y) = num {
-//             b.speed = 0.0;
-//             b.y = y;
-//         }
-//         return b;
-//     }
-// }
+use rphysics::screen::*;
 
 fn get_circles() -> Vec<Circle> {
-    let circ_1 = Circle::new(0.0, 400.0, 100.0, 20.0, 0.0);
-    let circ_2 = Circle::new(400.0, 400.0, 100.0, -20.0, 0.0);
+    let circ_1 = Circle::new(50.0, 50.0, 50.0, 60.0, 0.0);
+    let circ_2 = Circle::new(462.0, 462.0, 50.0, -60.0, 0.0);
     return vec![circ_1, circ_2];
 }
 
-fn update(circ_list: &mut Vec<Circle>, dt: f64) {
+fn update(circ_list: &mut Vec<Circle>, dt: f64, screen: &Screen) {
     for circ in circ_list {
         circ.update_pos(dt);
+        circ.check_bounds(screen.width(), screen.height());
     }
 }
 
+///gets center of the circle
+// fn get_center(x: f64, y: f64) -> (f64, f64) {}
+
+// ellipse(x,y,halfwidth,halfheight)  x,y are points where the ellips touch x and y axes
+
 fn main() {
-    let mut window: PistonWindow = WindowSettings::new("Hello World!", [512; 2])
-        .build()
-        .unwrap();
+    let screen = Screen::new(512.0, 512.0);
+
+    // initializing piston window
+    let mut window: PistonWindow = WindowSettings::new("Circles!", [512; 2]).build().unwrap();
 
     // list of circles to render
     let mut circ_list = get_circles();
+
     // render loop
     while let Some(e) = window.next() {
         // this is for rendering
@@ -55,9 +36,15 @@ fn main() {
             window.draw_2d(&e, |c, g, _| {
                 clear([0.5, 0.5, 0.5, 1.0], g);
                 for circ in &circ_list {
+                    // let cir = ellipse::circle(circ.x(), circ.y(), circ.r());
                     ellipse(
-                        [1.0, 0.0, 0.0, 1.0],                     // red color
-                        [circ.x(), circ.y(), circ.r(), circ.r()], // rectangle  [x,y,width,height]
+                        [1.0, 0.0, 0.0, 1.0], // red color
+                        [
+                            circ.x() - circ.r(),
+                            circ.y() - circ.r(),
+                            2.0 * circ.r(),
+                            2.0 * circ.r(),
+                        ],
                         c.transform,
                         g,
                     );
@@ -68,7 +55,7 @@ fn main() {
         if let Some(u) = e.update_args() {
             // update position according to speed after every unit of time in simulation
             // u->update object ;;;; u.dt ----> time elapsed in simulation
-            update(&mut circ_list, u.dt);
+            update(&mut circ_list, u.dt, &screen);
         }
     }
 }
