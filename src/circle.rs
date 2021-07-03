5deebfb5e2c25e494941f184dd7bb8fbfa8fd4ce
TheCodeYoda <smurf61988@gmail.com>
2021-07-03 19:23:45 +0530

    fixed check bound function in terms of negative velocities

--- src/circle.rs
+++ src/circle.rs
@@ -1,3 +1,5 @@
+use crate::screen::Check;
+
 pub struct Circle {
     x: f64,
     y: f64,
@@ -38,5 +40,30 @@ impl Circle {
     pub fn update_pos(&mut self, dt: f64) {
         self.x += self.vel_x * dt;
         self.y += self.vel_y * dt;
+    }
+
+    pub fn disp_coords(&self) {
+        println!("x: {} y: {}", self.x, self.y);
+    }
+}
+
+impl Check for Circle {
+    fn check_bounds(&mut self, width: f64, height: f64) {
+        if self.x + self.r > width {
+            self.x = width - self.r;
+            self.vel_x = 0.0;
+        }
+        if self.y + self.r > height {
+            self.y = height - self.r;
+            self.vel_y = 0.0;
+        }
+        if self.x - self.r < 0.0 {
+            self.x = self.r;
+            self.vel_x = 0.0;
+        }
+        if self.y - self.r < 0.0 {
+            self.y = self.r;
+            self.vel_y = 0.0;
+        }
     }
 }
