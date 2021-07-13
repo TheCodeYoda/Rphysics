5bc3109c2caf9c93d84b70fb2c2ac89337d51270
TheCodeYoda <smurf61988@gmail.com>
2021-07-13 19:02:08 +0530

    removed check_bounds function

--- src/collison.rs
+++ src/collison.rs
@@ -1,8 +1,6 @@
 use crate::circle::Circle;
-use crate::screen::Screen;
 
 extern crate nalgebra_glm as glm;
-use glm::*;
 
 /// finds euclidian distance between 2 points
 pub fn distance(x1: f64, y1: f64, x2: f64, y2: f64) -> f64 {
@@ -12,7 +10,6 @@ pub fn distance(x1: f64, y1: f64, x2: f64, y2: f64) -> f64 {
 
 // a trait for checking bounds of screen and readjusting the position of the shape accordingly
 pub trait Collision {
-    fn check_bounds(&mut self, screen: &Screen, e: f64) -> DVec2;
     fn is_colliding(&mut self, other: &mut Circle) -> bool;
     fn collide(&mut self, other: &mut Circle, e: f64, dt: f64);
 }
